9081171d6a7860f4f5ee64b0b30dbd56c1ec7e91
maksdy <diachenko.maksym@gmail.com>
2019-01-23 17:55:01 +0200

    Sorted values in console

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-var dataURL = 'tickets.json'
+var dataURL = 'tickets.json';
 
 var app = new Vue({
   el: '#app',
   data: {
-    userData: []
+    tickets: []
   },
   mounted() {
     var self = this
     $.getJSON(dataURL, function(data) {
-      self.userData = data.tickets;
+      self.tickets = data.tickets;
+      array = data.tickets;
+      for (var i=0;
+         i < array.sort(function(obj1, obj2){
+          return obj1.price-obj2.price;
+          }).length;
+         i++) {
+              console.log(array[i].price);
+          }
     });
   }
 })
