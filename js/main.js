4da8482207c1937889639c3428b6035bb3d48ce2
maksdy <diachenko.maksym@gmail.com>
2019-01-24 11:48:16 +0200

    Filteringarray in console by stops

--- js/main.js
+++ js/main.js
@@ -3,20 +3,24 @@ var dataURL = 'tickets.json';
 var app = new Vue({
   el: '#app',
   data: {
-    tickets: []
+    tickets: [],
+    defaultCheck: true
   },
   mounted() {
     var self = this
     $.getJSON(dataURL, function(data) {
       self.tickets = data.tickets;
       array = data.tickets;
+      // sorting ascending price
       for (var i=0;
          i < array.sort(function(obj1, obj2){
           return obj1.price-obj2.price;
           }).length;
-         i++) {
-              console.log(array[i].price);
-          }
+         i++) {};
+
+         array = array.filter(ticket => ticket.stops == 0);
+         console.log(array);
+
     });
   }
 })
