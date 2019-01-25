562be19d5723a2723d618888ae9270a5c93f7816
maksdy <diachenko.maksym@gmail.com>
2019-01-25 11:44:09 +0200

    Filter by stops works!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 var dataURL = 'tickets.json';
 
+
+
+// <div class="col col-lg-8" id="app">
+
 var app = new Vue({
   el: '#app',
   data: {
     tickets: [],
+    zeroChecked: false,
+    oneChecked: false,
+    twoChecked: false,
+    threeChecked: false
   },
   mounted() {
     var self = this
@@ -12,9 +20,6 @@ var app = new Vue({
       array = data.tickets;
       // sorting ascending price
       array.sort(function(obj1, obj2){return obj1.price-obj2.price});
-
-      data.tickets = array.filter(ticket => ticket.stops == 0);
-
     });
-  }
+  },
 })
